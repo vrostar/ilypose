6dc842f0b76246f9492651a8d8e7c8c9ff017cee
Admin <mistermustache33@gmail.com>
2023-03-19 19:33:58 +0100

    Initial commit

---
+++ poser/src/index.js
@@ -0,0 +1,182 @@
+let model
+let videoWidth, videoHeight
+let ctx, canvas
+let poseData = []
+const k = 3
+const machine = new kNear(k)
+const log = document.querySelector("#array")
+const VIDEO_WIDTH = 720
+const VIDEO_HEIGHT = 405
+
+// buttons
+const IBtn = document.querySelector("#i");
+const LBtn = document.querySelector("#l");
+const YBtn = document.querySelector("#y");
+const GuessBtn = document.querySelector("#guess");
+
+IBtn.addEventListener("click", () => poseI());
+LBtn.addEventListener("click", () => poseL());
+YBtn.addEventListener("click", () => poseY());
+GuessBtn.addEventListener("click", () => poseGuess());
+
+function poseI(){
+machine.learn(poseData, "I")
+    console.log(poseData)
+    console.log("Pinky finger (I)")
+}
+
+function poseL(){
+    machine.learn(poseData, "L")
+    console.log(poseData)
+    console.log("Index and thumb (L)")
+}
+
+function poseY(){
+    machine.learn(poseData, "Y")
+    console.log(poseData)
+    console.log("Shaka sign, thumb and Pinky (Y)")
+}
+
+function poseGuess(){
+    let prediction = machine.classify(poseData)
+    console.log(`You got the ${prediction}`)
+    document.getElementById("prediction").innerHTML = `You are posing the:${prediction}`;
+}
+
+//
+// start de applicatie
+//
+async function main() {
+    model = await handpose.load()
+    const video = await setupCamera()
+    video.play()
+    startLandmarkDetection(video)
+}
+
+//
+// start de webcam
+//
+async function setupCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error(
+            "Webcam not available"
+        )
+    }
+
+    const video = document.getElementById("video")
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+            facingMode: "user",
+            width: VIDEO_WIDTH,
+            height: VIDEO_HEIGHT
+        }
+    })
+    video.srcObject = stream
+
+    return new Promise(resolve => {
+        video.onloadedmetadata = () => {
+            resolve(video)
+        }
+    })
+}
+
+//
+// predict de vinger posities in de video stream
+//
+async function startLandmarkDetection(video) {
+
+    videoWidth = video.videoWidth
+    videoHeight = video.videoHeight
+
+    canvas = document.getElementById("output")
+
+    canvas.width = videoWidth
+    canvas.height = videoHeight
+
+    ctx = canvas.getContext("2d")
+
+    video.width = videoWidth
+    video.height = videoHeight
+
+    ctx.clearRect(0, 0, videoWidth, videoHeight)
+    ctx.strokeStyle = "red"
+    ctx.fillStyle = "red"
+
+    ctx.translate(canvas.width, 0)
+    ctx.scale(-1, 1) // video omdraaien omdat webcam in spiegelbeeld is
+
+    predictLandmarks()
+}
+
+//
+// predict de locatie van de vingers met het model
+//
+async function predictLandmarks() {
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight, 0, 0, canvas.width, canvas.height)
+    // prediction!
+    const predictions = await model.estimateHands(video) // ,true voor flip
+    if (predictions.length > 0) {
+        drawHand(ctx, predictions[0].landmarks, predictions[0].annotations)
+    }
+    // 60 keer per seconde is veel, gebruik setTimeout om minder vaak te predicten
+    requestAnimationFrame(predictLandmarks)
+    // setTimeout(()=>predictLandmarks(), 1000)
+}
+
+
+//
+// teken hand en vingers met de x,y coordinaten. de z waarde tekenen we niet.
+//
+function drawHand(ctx, keypoints, annotations) {
+    // toon alle x,y,z punten van de hele hand in het log venster
+    log.innerText = keypoints.flat()
+    poseData = []
+
+    // punten op alle kootjes kan je rechtstreeks uit keypoints halen 
+    for (let i = 0; i < keypoints.length; i++) {
+        const y = keypoints[i][0]
+        const x = keypoints[i][1]
+        poseData.push(y)
+        poseData.push(x)
+        drawPoint(ctx, x - 2, y - 2, 3)
+    }
+
+    // palmbase als laatste punt toevoegen aan elke vinger
+    let palmBase = annotations.palmBase[0]
+    for (let key in annotations) {
+        const finger = annotations[key]
+        finger.unshift(palmBase)
+        drawPath(ctx, finger, false)
+    }
+}
+
+//
+// teken een punt
+//
+function drawPoint(ctx, y, x, r) {
+    ctx.beginPath()
+    ctx.arc(x, y, r, 0, 2 * Math.PI)
+    ctx.fill()
+}
+//
+// teken een lijn
+//
+function drawPath(ctx, points, closePath) {
+    const region = new Path2D()
+    region.moveTo(points[0][0], points[0][1])
+    for (let i = 1; i < points.length; i++) {
+        const point = points[i]
+        region.lineTo(point[0], point[1])
+    }
+
+    if (closePath) {
+        region.closePath()
+    }
+    ctx.stroke(region)
+}
+
+//
+// start
+//
+main()
